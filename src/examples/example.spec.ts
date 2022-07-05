aad9c9b644b491c0646db942c61541c3afd03bfb
Jeff Costa <jeffersomcst2@gmail.com>
2022-07-04 23:33:01 -0300

    criacao dos primeiros testes

---
+++ src/examples/example.spec.ts
@@ -0,0 +1,6 @@
+describe('Tipos Primitivos', () => {
+    it('should test jest assetions', () => {
+        const number:Number = 10;
+        expect(number).toBe(10);
+    })
+})
--- src/services/persistency.spec.ts
+++ src/services/persistency.spec.ts
@@ -1,25 +1,22 @@
-import { Persistency } from './persistency';
+import { Persistency } from "./persistency";
 
-describe('Persistency', () => {
-  afterEach(() => jest.clearAllMocks());
+describe("Persistency" , () => {
+    it("should return undefined", () => {
+        const sut:Persistency = new Persistency;
+        expect(sut.saveOrder()).toBeUndefined();
+    }) 
+    it("should call console.log", () => {
+        const sut:Persistency = new Persistency;
+        const consoleSpy = jest.spyOn(console, 'log');
+        sut.saveOrder();
+        expect(consoleSpy).toHaveBeenCalledTimes(1);
 
-  it('should return undefined', () => {
-    // System under test
-    const sut = new Persistency();
-    expect(sut.saveOrder()).toBeUndefined();
-  });
+    })
+    it("should call console.log with 'Pedido salvo com sucesso'", () => {
+        const sut:Persistency = new Persistency;
+        const consoleSpy = jest.spyOn(console, 'log');
+        sut.saveOrder();
+        expect(consoleSpy).toHaveBeenCalledWith('Pedido salvo com sucesso...');
 
-  it('should call console.log once', () => {
-    const sut = new Persistency();
-    const consoleSpy = jest.spyOn(console, 'log');
-    sut.saveOrder();
-    expect(consoleSpy).toHaveBeenCalledTimes(1);
-  });
-
-  it('should call console.log with "Pedido salvo com sucesso..."', () => {
-    const sut = new Persistency();
-    const consoleSpy = jest.spyOn(console, 'log');
-    sut.saveOrder();
-    expect(consoleSpy).toHaveBeenCalledWith('Pedido salvo com sucesso...');
-  });
-});
+    })
+})
